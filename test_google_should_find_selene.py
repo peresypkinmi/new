7ad2d088cbf62cb037f82afbb8102e1fbb4c3e23
peresypkin.maQA <peresypkin.ma@sibvaleo.com>
2023-03-05 15:27:36 +0700

    добавил conftest с фикстурами настроек браузера и открытием стартовой страницы. добавил тест с негативной проверкой

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -1,9 +1,11 @@
 from selene.support.shared import browser
 from selene import be, have
 
+
 def test_searchresult_is_exist(open_base_page):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
 
 def test_searchresult_is_no_exist(open_base_page):
     browser.element('[name="q"]').should(be.blank).type('lkjhgjhgjhgjhfchxgfdh').press_enter()
